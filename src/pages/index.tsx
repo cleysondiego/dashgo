fb8f346ac4fa1b565039b344e173f612cc73809c
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-07-13 22:20:55 -0300

    Aula: Form de criação de usuário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ type SignInFormData = {
   password: string;
 };
 
-
 const signInFormSchema = yup.object().shape({
   email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
   password: yup.string().required('Senha obrigatória'),
